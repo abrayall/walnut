2c529ae776502c6d81449118dee63346098adf8c
Aaron Brayall <abrayall@yahoo.com>
2020-11-25 10:45:50 -0500

    updating with head to head matchup comparison test

--- test3.py
+++ test3.py
@@ -1,6 +1,8 @@
 import pandas
 
 print(pandas.read_html('https://www.teamrankings.com/nfl/')[0])
-
-
 print(pandas.read_html('https://www.teamrankings.com/nfl/ranking/predictive-by-other/')[0])
+
+
+tables = pandas.read_html('https://www.teamrankings.com/nfl/matchup/jaguars-packers-week-10-2020/stats')
+print(tables[0][tables[0].iloc[:, 0] == 'Points/Game'].iloc[0, 1].replace('(', '').replace(')', '').replace('#', '').split(' '))
